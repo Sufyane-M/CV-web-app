2d1a0fa9f6885ab13d685172c5c81415aef0dcb4
Sufyane Majdouli <majdouli.sufyane@gmail.com>
2025-08-25 19:05:43 +0200

    ciao 9

--- api/stripe/create-checkout-session.js
+++ api/stripe/create-checkout-session.js
@@ -157,7 +157,7 @@ export default async function handler(req, res) {
     }
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card', 'paypal'],
+      payment_method_types: ['card'],
       line_items: [
         {
           price_data: {
